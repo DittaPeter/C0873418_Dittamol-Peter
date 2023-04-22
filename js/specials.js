bca4239a54035895ef9f295f579d19d0a7541de8
DittaPeter <ditta254@gmail.com>
2023-04-22 02:43:07 -0400

    Updates

--- js/specials.js
+++ js/specials.js
@@ -4,7 +4,9 @@
     var itemWidth = items.outerWidth(true);
     var currentPosition = 0;
     var maxPosition = items.length - 4;
-    
+
+    var slider = $("#image_list");                     // slider = ul element
+	  var leftProperty, newleftProperty;
     
     $(".next").click(function() {
       if (currentPosition < maxPosition) {
@@ -19,4 +21,40 @@
         carousel.animate({ left: "+=" + itemWidth + "px" }, "fast");
       }
     });
+
+    $('.product').click(function() {
+      $(this).animate({backgroundColor: 'gray'}, 1000);
+    });
+
+    $("#seach-button").click(function () {
+      var searchTerm = $('#search-image').val();
+      if (!searchTerm) {
+        alert("Please enter a term to search");
+      } else {
+        var flickrApiUrl = 'https://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=?&tags=' + searchTerm;
+        $.ajax({
+          url: flickrApiUrl, // replace the URL with your API endpoint
+          type: 'GET',
+          dataType: 'json', // expected data type
+          success: function(responseData) {
+            if (responseData && responseData.items) {
+              var divData = '';
+              (responseData.items).forEach(function(item, i) {
+                if (i < 6) {
+                  divData += "<div class=image-flickr><h2>" + item.title + "</h2><img src=" + item.media.m + "><div class=tag>" + item.tags + "</div></div>";
+                }
+              });
+              $("#images-flickr").html(divData);
+              $("#images-flickr").removeClass('hide');
+            }
+          },
+          error: function(xhr, status, error) {
+            console.log('AJAX request failed: ' + status + ', ' + error);
+          }
+        });
+        
+      }
+
+    });
+
   });
